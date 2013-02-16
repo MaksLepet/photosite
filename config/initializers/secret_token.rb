9354131020d70177d6c8c7400e3b0cff8287f08a
Nikita Andreev <nsa@nsa.in.ua>
2013-02-16 18:21:37 +0200

    Initial commit - new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photosite::Application.config.secret_token = 'f891bb8c51888a6427dee635f7d623674fac5aebbc20156d03d6103828ca9237e9c5036cb9f99959c70692accc26e6d4a9e9e1d745bda5bf4e6f7fcac51b0810'
